3f3f10ea6c025a9e7a8feab0a7b03c3cb33fa362
hazemhemaily <hazem.hemily@gmail.com>
2021-01-29 18:38:32 +0200

    useState / useEffect hooks revision

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(() => 0);
+  const [title, setTitle] = useState(() => "");
+  useEffect(() => {
+   
+    console.log(" component did mount");
+    // return () => {
+    //   console.log("component did unmount");
+    // };
+  }, [count]);
+  function handleTitle () { 
+    setTitle("useEffect hook revision")
+    document.title = title; 
+  }
+  function handleCount() { 
+    document.title = `you clicked ths button ${count+1} times`; 
+    setCount(count+1)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{`you clicked ths button ${count} times`}</p>
+      <div>
+        <button onClick={handleCount}>set count</button>
+        <button onClick={handleTitle}>set title</button>
+      </div>
     </div>
   );
 }
